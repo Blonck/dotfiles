e490d51ae41bf1b636b1e44241ad4a4393fab638
Martin Marenz <Martin.Marenz@tomtom.com>
2019-07-08 07:01:44 +0200

    Add TTT notebook as zsh host.

---
+++ zsh/host/de2lxl-527923.sh
@@ -0,0 +1,17 @@
+PATH="$HOME/local/bin:$HOME/.cargo/bin/:$HOME/local/julia-1.0.2/bin:$PATH"
+
+use_conda ()
+{
+    # !! Contents within this block are managed by 'conda init' !!
+    __conda_setup="$('/home/marenz/local/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    if [ $? -eq 0 ]; then
+        eval "$__conda_setup"
+    else
+        if [ -f "/home/marenz/local/anaconda3/etc/profile.d/conda.sh" ]; then
+            . "/home/marenz/local/anaconda3/etc/profile.d/conda.sh"
+        else
+            export PATH="/home/marenz/local/anaconda3/bin:$PATH"
+        fi
+    fi
+    unset __conda_setup
+}
