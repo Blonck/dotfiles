7deece93b9936c1d3d155a1ef8834b40300a22f0
Martin Marenz <martin.marenz@webfleet.com>
2023-01-10 06:58:13 +0100

    fix lsp config for neovim

--- nvim/lua/lsp-config/init.lua
+++ nvim/lua/lsp-config/init.lua
@@ -1,13 +1,25 @@
 -- Set up lspconfig.
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- lsp
+local on_attach = function(client, bufnr)
+    local opts = { noremap=true, silent=true, buffer=bufnr }
+
+    vim.keymap.set('n', '<leader>j', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', '<leader>R', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', '<leader>l', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+end
+
 require('lspconfig')['pyright'].setup{
-    on_attach = lsp_on_attach,
+    on_attach = on_attach,
     capabilities = capabilities
 }
 
 require('lspconfig')['rust_analyzer'].setup{
-    on_attach = lsp_on_attach,
+    on_attach = on_attach,
     capabilities = capabilities,
     settings = {
       ["rust-analyzer"] = {}
