a8af6cc4f6da5c7b669dd4b29135a29f4fec12fa
Martin Marenz <martin.marenz@gmail.com>
2023-12-19 07:56:53 +0100

    [nvim] set python provider to global one

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -4,7 +4,7 @@ local set = vim.opt
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 vim.g.netrw_winsize = 30
-
+vim.g.python3_host_prog = "/usr/bin/python3"
 
 -- general
 set.mouse = 'a'                 -- enable mouse support
